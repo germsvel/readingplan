bcc4f5d35ec2aa3ec5e3b09aaddfd9dbbf2c0ead
German Velasco <germsvel@gmail.com>
2013-11-16 10:18:01 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ReadingPlan::Application.config.secret_key_base = '7feca4fcd6ae52226d8c8fecb1f5449adccc9cc7e391f777bc89351abab123132c5d5701e968a69bbba0fb48cd517d57f684c38c611c2ccde0c72c0661ae1830'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ReadingPlan::Application.config.session_store :cookie_store, key: '_ReadingPlan_session'
